f800065d9b56abe5c16ba9abda408b4e704a3850
Owner <braydens.coding@gmail.com>
2025-03-11 00:46:57 -0500

    Video 3: Typecasting

---
+++ typecasting.py
@@ -0,0 +1,31 @@
+# Typecasting is the process of converting a variable from one data type to another
+#           str(), int(), float(), bool()
+
+name = "Brayden"
+age = 18
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa)
+age = float(age)
+
+print(gpa)
+print(age)
+
+#age = str(age)
+
+print(type(age))
+
+#age += "1"
+
+print(age)
+
+name = bool(name)
+
+print(name)
+
+name = ""
+
+name = bool(name)
+
+print(name)
