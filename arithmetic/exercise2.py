766aa00917b9cbfad47790853958f765bf2df22d
Owner <braydens.coding@gmail.com>
2025-03-12 20:07:38 -0500

    Videos 7 & 8 complete

---
+++ arithmetic/exercise2.py
@@ -0,0 +1,9 @@
+import math
+
+# Calculate the area of a circle
+
+radius = float(input("Enter the radius of a circle: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"The area of the circle is {round(area, 2)}cm^2")
