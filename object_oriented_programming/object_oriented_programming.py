499e6ee7a6f5fc22dbc0d3d36759dc5928abc049
Owner <braydens.coding@gmail.com>
2025-04-08 20:19:38 -0500

    Video 46: Object oriented programming

---
+++ object_oriented_programming/object_oriented_programming.py
@@ -0,0 +1,24 @@
+# object = A "bundle" of related attributes (variables) and methods (functions)
+#          Ex. phone, cup, book
+#          You need a "class" to create many objects
+
+# class = (blueprint) used to design the structure and layout of an object
+
+from car import Car
+
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+car3 = Car("Charger", 2026, "yellow", True)
+
+print(car3.model)
+print(car3.year)
+print(car3.color)
+print(car3.for_sale)
+
+car1.drive()
+car1.stop()
+
+car1.describe()
+car2.describe()
+car3.describe()
